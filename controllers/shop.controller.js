ea61dfbddaf4a2085bd08823b38b9cc910b00f90
ashish <ashishk4371@gmail.com>
2019-06-24 03:37:47 +0530

    pagination

--- controllers/shop.controller.js
+++ controllers/shop.controller.js
@@ -5,11 +5,24 @@ const path = require("path");
 const PDFDocument = require("pdfkit");
 const fs = require("fs");
 
+const ITEMS_PER_PAGE = 2;
+
 // get products route controller
 
 // get the index page
 exports.getIndexPage = (req, res, next) => {
+  const page = +req.query.page || 1;
+
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then(totalProducts => {
+      totalItems = totalProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(productDoc => {
       res.status(201).render("shop/index", {
         pageTitle: "Shop",
@@ -17,7 +30,13 @@ exports.getIndexPage = (req, res, next) => {
         products: productDoc,
         isAuthenticated: req.session.isLoggedIn,
         csrfToken: req.csrfToken(),
-        user: req.user
+        user: req.user,
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
